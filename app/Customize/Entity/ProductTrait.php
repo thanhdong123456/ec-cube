b2cb7a85b95fe75dc6cd95323861479b1d38d87d
dongnt <dongcoi.tk@gmail.com>
2025-02-21 17:29:53 +0700

    feat: product free shipping

--- app/Customize/Entity/ProductTrait.php
+++ app/Customize/Entity/ProductTrait.php
@@ -6,14 +6,43 @@ use Doctrine\ORM\Mapping as ORM;
 use Eccube\Annotation\EntityExtension;
 
 /**
-  * @EntityExtension("Eccube\Entity\Product")
+ * @EntityExtension("Eccube\Entity\Product")
  */
 trait ProductTrait
 {
-    /**
-     * @var \Doctrine\Common\Collections\Collection
-     *
-     * @ORM\OneToMany(targetEntity="Eccube\Entity\OrderItem", mappedBy="Product", cascade={"persist"})
-     */
-    private $OrderItems;
+  /**
+   * @var \Doctrine\Common\Collections\Collection
+   *
+   * @ORM\OneToMany(targetEntity="Eccube\Entity\OrderItem", mappedBy="Product", cascade={"persist"})
+   */
+  public $OrderItems;
+
+  /**
+   * @var boolean
+   *
+   * @ORM\Column(name="free_shipping", type="boolean", options={"default": false})
+   */
+  public $free_shipping;
+
+  /**
+   * Get the value of free_shipping
+   *
+   * @return bool
+   */
+  public function getFreeShipping(): bool
+  {
+    return $this->free_shipping;
+  }
+
+  /**
+   * Set the value of free_shipping
+   *
+   * @param bool $free_shipping
+   * @return self
+   */
+  public function setFreeShipping(bool $free_shipping): self
+  {
+    $this->free_shipping = $free_shipping;
+    return $this;
+  }
 }
